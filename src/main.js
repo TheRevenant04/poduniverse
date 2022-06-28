099eeab7f03337f014d3966600b5c25b224e0503
Pritesh Parmar <priteshparmar1999@gmail.com>
2022-06-28 20:05:20 +0200

    Added new packages
    Added the dayjs npm package.
    Added custom configuration for dayjs in the main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,53 @@
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import updateLocale from 'dayjs/plugin/updateLocale';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.use(Buefy);
+
+dayjs.extend(duration);
+const config = {
+  thresholds: [
+    { l: 's', r: 59, d: 'second' },
+    { l: 'm', r: 1 },
+    { l: 'mm', r: 59, d: 'minute' },
+    { l: 'h', r: 1 },
+    { l: 'hh', r: 23, d: 'hour' },
+    { l: 'd', r: 1 },
+    { l: 'dd', r: 29, d: 'day' },
+    { l: 'M', r: 1 },
+    { l: 'MM', r: 11, d: 'month' },
+    { l: 'y' },
+    { l: 'yy', d: 'year' },
+  ],
+};
+dayjs.extend(relativeTime, config);
+dayjs.extend(updateLocale);
+dayjs.updateLocale('en', {
+  relativeTime: {
+    future: 'in %s',
+    past: '%s ago',
+    s: '%d seconds',
+    m: '1 minute',
+    mm: '%d minutes',
+    h: '1 hour',
+    hh: '%d hours',
+    d: '1 day',
+    dd: '%d days',
+    M: '1 month',
+    MM: '%d months',
+    y: '1 year',
+    yy: '%d years',
+  },
+});
+
+Vue.use(dayjs);
 
 Vue.config.productionTip = false;
 
