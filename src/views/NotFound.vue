0ff8ff72b12e7e8b948a51d4aeab4e9c2d8fa8ac
Pritesh Parmar <priteshparmar1999@gmail.com>
2022-07-19 19:43:45 +0200

    Added a the NotFound view that is displayed if an invalid url is accessed.

---
+++ src/views/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="columns min-page-height is-mobile is-centered is-vcentered">
+    <p class="title is-size-4 has-text-white">Page not found</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'NotFound',
+};
+</script>
