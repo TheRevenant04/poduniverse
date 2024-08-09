40d6f70c4d807d8ad72de7f3ccbe865b0f7a3de1
Pritesh Parmar <priteshparmar1999@gmail.com>
2024-08-09 23:27:14 +0200

    build: add prime-icons

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 import PrimeVue from "primevue/config";
 import Aura from "@primevue/themes/aura";
 import "@/assets/css/index.css";
+import "primeicons/primeicons.css";
 
 import App from "./App.vue";
 import router from "./router";
@@ -14,7 +15,7 @@ app.use(PrimeVue, {
     preset: Aura,
     options: {
       prefix: "p",
-      darkModeSelector: "system",
+      darkModeSelector: ".dark-theme",
       cssLayer: {
         name: "primevue",
         order: "tailwind-base, primevue, tailwind-utilities"
