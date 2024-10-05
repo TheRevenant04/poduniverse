cd0856b9a1e69c1d42b953a25c5e06a28cd949d1
Pritesh Parmar <priteshparmar1999@gmail.com>
2024-10-05 20:17:46 +0200

    feat: add dtos

---
+++ src/dtos/Episode.js
@@ -0,0 +1,13 @@
+class Episode {
+  constructor(id, title, description, datePublishedPretty, duration, link, imageUrl) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.datePublished = datePublishedPretty;
+    this.duration = duration;
+    this.link = link;
+    this.imageUrl = imageUrl;
+  }
+}
+
+export default Episode;
---
+++ src/dtos/Podcast.js
@@ -0,0 +1,13 @@
+class Podcast {
+  constructor(id, title, author, description, owner, link, imageUrl, categories) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.description = description;
+    this.owner = owner;
+    this.link = link;
+    this.imageUrl = imageUrl;
+    this.categories = categories;
+  }
+}
+export default Podcast;
