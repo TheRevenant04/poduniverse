77006c64b8f61a1d8802b347e10b0f87891233d9
Pritesh Parmar <priteshparmar1999@gmail.com>
2024-10-05 20:22:37 +0200

    refactor: add primevue component implementation

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,62 +1,21 @@
 <template>
-  <section>
-    <b-field class="p-2">
-      <b-input
-        v-model="searchTerm"
-        placeholder="Find an amazing podcast..."
-        rounded
-        type="search"
-        icon-pack="fas"
-        icon="search"
-        size="is-medium"
-        icon-clickable
-        @icon-click="search"
-        v-on:keyup.native.enter="search"
-        custom-class="has-background-grey-dark has-text-white is-size-6-mobile"
-      >
-      </b-input>
-    </b-field>
-  </section>
+  <InputGroup class="w-full md:w-1/2">
+    <InputText placeholder="Find an amazing podcast" size="large" v-model="searchTerm" />
+    <Button icon="pi pi-search" severity="primary" @click="search" />
+  </InputGroup>
 </template>
 
-<script>
-import { mapActions, mapGetters } from 'vuex';
+<script setup>
+import { useSearchStore } from "@/stores/search";
+import { computed } from "vue";
 
-export default {
-  data() {
-    return {
-      searchTerm: '',
-    };
-  },
-  computed: {
-    ...mapGetters({
-      computedSearchTerm: 'searchString',
-      podcastSearchResultList: 'podcastSearchResultList',
-    }),
-  },
-  watch: {
-    computedSearchTerm: {
-      immediate: true,
-      handler(value) {
-        this.searchTerm = value;
-      },
-    },
-  },
-  methods: {
-    setSearchString() {
-      this.$store.commit('setSearchString', this.searchTerm);
-    },
-    async search() {
-      try {
-        this.setSearchString();
-        if (this.searchTerm !== '') {
-          await this.fetchPodcastsBySearchTerm(this.searchTerm);
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    },
-    ...mapActions(['fetchPodcastsBySearchTerm']),
-  },
+const searchStore = useSearchStore();
+const searchTerm = computed({
+  get: () => searchStore.searchTerm,
+  set: (value) => (searchStore.searchTerm = value)
+});
+
+const search = async () => {
+  searchStore.fetchPodcastsBySearchTerm(searchTerm.value);
 };
 </script>
