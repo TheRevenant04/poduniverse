6a0589d4cef3fd773a2dbb065b888d78ae09b12f
Pritesh Parmar <priteshparmar1999@gmail.com>
2024-10-05 20:18:35 +0200

    refactor: add tailwind implementations for components

--- src/components/PodcastCardGrid.vue
+++ src/components/PodcastCardGrid.vue
@@ -1,28 +1,21 @@
 <template>
-  <div class="columns is-multiline is-mobile p-2">
-    <div
-      v-for="(podcast, key) in podcastSearchResultList"
-      :key="key"
-      class="column is-6-mobile is-3-tablet is-2-desktop"
-    >
+  <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+    <div v-for="(podcast, key) in podcastSearchResultList" :key="key" class="px-3 mb-5">
       <router-link :to="{ name: 'podcast', params: { id: podcast.id } }">
-        <podcast-card
-          :id="podcast.id"
-          :title="podcast.title"
-          :image="podcast.imageUrl"
-        />
+        <podcast-card :id="podcast.id" :title="podcast.title" :image="podcast.imageUrl" />
       </router-link>
     </div>
   </div>
 </template>
-<script>
-import { mapGetters } from 'vuex';
-import PodcastCard from './PodcastCard.vue';
 
-export default {
-  components: { PodcastCard },
-  computed: {
-    ...mapGetters(['podcastSearchResultList']),
-  },
-};
+<script setup>
+import { useSearchStore } from "@/stores/search";
+import PodcastCard from "./PodcastCard.vue";
+import { computed } from "vue";
+
+const searchStore = useSearchStore();
+
+const podcastSearchResultList = computed(() => {
+  return searchStore.podcastSearchResultList;
+});
 </script>
