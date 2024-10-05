ed4516246eb4305df4b54c21e1c32db1f8b012f4
Pritesh Parmar <priteshparmar1999@gmail.com>
2024-10-05 20:31:40 +0200

    refactor: add content on landing page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,18 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <div v-if="searchStore.showSearchResults" class="flex flex-col items-center">
+    <search-bar />
+    <div class="flex flex-col items-center mt-5 md:mt-10">
+      <span class="text-xl md:text-2xl mb-5 md:mb-10">Results</span>
+      <podcast-card-grid />
+    </div>
+  </div>
+  <div v-else class="flex justify-center items-center h-full">
+    <search-bar />
+  </div>
 </template>
+<script setup>
+import { useSearchStore } from "@/stores/search";
+import SearchBar from "../components/SearchBar.vue";
+import PodcastCardGrid from "@/components/PodcastCardGrid.vue";
+const searchStore = useSearchStore();
+</script>
