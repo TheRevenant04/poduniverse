b5a4f1be5005491700706f43a90935cccf84ccbc
Pritesh Parmar <priteshparmar1999@gmail.com>
2024-10-06 20:21:25 +0200

    refactor: add dayjs config

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import PrimeVue from "primevue/config";
 import Aura from "@primevue/themes/aura";
 import "@/assets/css/index.css";
 import "primeicons/primeicons.css";
+import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration";
+import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
 
 import App from "./App.vue";
 import router from "./router";
@@ -23,6 +27,44 @@ app.use(PrimeVue, {
     }
   }
 });
+
+dayjs.extend(duration);
+const config = {
+  thresholds: [
+    { l: "s", r: 59, d: "second" },
+    { l: "m", r: 1 },
+    { l: "mm", r: 59, d: "minute" },
+    { l: "h", r: 1 },
+    { l: "hh", r: 23, d: "hour" },
+    { l: "d", r: 1 },
+    { l: "dd", r: 29, d: "day" },
+    { l: "M", r: 1 },
+    { l: "MM", r: 11, d: "month" },
+    { l: "y" },
+    { l: "yy", d: "year" }
+  ]
+};
+dayjs.extend(relativeTime, config);
+dayjs.extend(updateLocale);
+dayjs.updateLocale("en", {
+  relativeTime: {
+    future: "in %s",
+    past: "%s ago",
+    s: "%d seconds",
+    m: "1 minute",
+    mm: "%d minutes",
+    h: "1 hour",
+    hh: "%d hours",
+    d: "1 day",
+    dd: "%d days",
+    M: "1 month",
+    MM: "%d months",
+    y: "1 year",
+    yy: "%d years"
+  }
+});
+
+app.use(dayjs);
 
 app.use(router);
 app.use(createPinia());
