230b80be408e46ae12b19731e81fe09361a297c3
Pritesh Parmar <priteshparmar1999@gmail.com>
2024-10-06 20:22:52 +0200

    perf: update code to reduce component re-renders

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,11 +1,11 @@
 <template>
-  <loader v-if="searchStore.showPageLoader" />
-  <div v-else class="flex flex-col min-h-screen">
+  <loader v-show="searchStore.showPageLoader" />
+  <div v-show="!searchStore.showPageLoader" class="flex flex-col min-h-screen">
     <app-navbar class="flex-shrink-0" />
     <div
       class="flex-grow flex justify-center bg-custom-gradient-light dark:bg-custom-gradient-dark"
     >
-      <div class="w-[95%] p-4">
+      <div class="w-[98%] md:w-[95%] p-4">
         <router-view />
       </div>
     </div>
@@ -16,7 +16,7 @@
 import AppNavbar from "./AppNavbar.vue";
 import AppFooter from "./AppFooter.vue";
 import Loader from "../components/Loader.vue";
-import { useSearchStore } from "@/stores/search";
+import { usePodcastStore } from "@/stores/podcasts";
 
-const searchStore = useSearchStore();
+const searchStore = usePodcastStore();
 </script>
