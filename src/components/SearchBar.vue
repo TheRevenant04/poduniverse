915a04cd63fef7695e1786abbe76d58d8411f483
Pritesh Parmar <priteshparmar1999@gmail.com>
2024-10-10 21:27:24 +0200

    fix: fix action for search button

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,7 +2,7 @@
   <form class="w-full md:w-1/2" @submit.prevent="search">
     <InputGroup>
       <InputText placeholder="Find an amazing podcast" size="large" v-model="searchTerm" />
-      <Button icon="pi pi-search" severity="primary" />
+      <Button type="submit" icon="pi pi-search" severity="primary" />
     </InputGroup>
   </form>
 </template>
