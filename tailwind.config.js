26392a6b666e70d3fd724d2c6fc65dfab0221a9c
Pritesh Parmar <priteshparmar1999@gmail.com>
2024-10-12 18:29:49 +0200

    style: add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ import tailwindcssPrimeui from "tailwindcss-primeui";
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"]
+      }
+    }
   },
   plugins: [tailwindcssPrimeui]
 };
